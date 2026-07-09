7424a2153e325d7ac99ccbd1854b228d530d33a5
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-07-09 15:40:18 +0330

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,81 @@
+# print("hello")
+
+"""
+hi this is session 1 
+and teach base of python
+rajaii teachers 
+"""
+
+# firstname = "amir hossein"
+
+# input
+# name = input("please enter name: ")
+# print(name)
+
+# print("amir hossein",end=" ")
+# print("a Teacher")
+# print("from mazandaran")
+
+
+# name = "amir hossein"
+# print(name)
+# print(type(name))
+
+# print(name[3])
+# print(name[4])
+
+# Slicing
+# print(name[0:4])
+# print(name[:4])
+# print(name[5:])
+# print(name[:])
+# print(name[-1])
+
+# print(name[0:11:2])
+# print(name[::-1])
+
+
+# name = "amir"
+# print(name.upper())
+
+# city = "BABOL"
+# print(city.lower())
+
+# message = "hi im a teacher"
+# print(message.title())
+
+# firstName = "amir reza"
+# print(firstName.replace("r","h"))
+
+# products = "apple,xiaomi,samsung,pocco"
+# print(products.split(","))
+
+# username = " amir"
+# print(username.lstrip())
+
+# F-string
+# firstname = "amir"
+# lastname = "hajitabar"
+# job = "teacher"
+
+# print(f"hello my name is {firstname} {lastname} and a {job}")
+# print("hello my name is {} {} and a  {}".format(firstname,lastname,job))
+# print("amirhossein\nhajitabar")
+# print("amirhossein\thajitabar")
+
+# a=5
+# b=2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# money = 1_000_000_000
+
+a = int(input("please enter number: "))
+print(type(a))
+print(a*5)
