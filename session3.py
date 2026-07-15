47034036b9c7dc2c3670f69f7c3dd4fb0f70672e
amirhossein hajitabar <amirhajitabar2@gmail.com>
2026-07-15 11:18:32 +0330

    Create session3.py

---
+++ session3.py
@@ -0,0 +1,69 @@
+# names = ["amir","ali","reza","ahmad"]
+# for name in names:
+#     print(name)
+# print("end of the loop")
+
+
+# names = ["amir","ali","reza","ahmad"]
+# for name in names:
+#     print(f"welcome mr {name}")
+
+
+# welcome mr amir
+# welcome mr ali
+# welcome mr reza
+# welcome mr ahmad
+
+
+# numbers = [10,12,15,17,6,9,3]
+"""
+10 --> zoj
+12 -->zoj
+15 --> fard
+.....
+"""
+# for number in numbers:
+#     if number % 2 ==0:
+#         print(f"{number} --> even")
+#     else:
+#         print(f"{number} --> odd")
+
+# print("end")
+
+
+# numbers = [10,12,15,17,6,9,3]
+# sum = 0
+# for number in numbers:
+#     # sum = sum + number 
+#     sum += number
+#     print(sum,end="   ")
+
+# print("")
+
+
+
+
+# for i in range(0,20):
+#     print("hello")
+
+
+# number = int(input("please enter number: "))
+# # from 0 to number 
+# for i in range(1,number+1,2):
+#     print(i)
+
+
+# for i in range(0,10):
+#     if i==6:
+#         break
+#     print(i)
+# print("out of loop")
+
+
+for i in range(0,10):
+    if i==6:
+        continue
+    print(i)
+
+print("out of loop")
+
